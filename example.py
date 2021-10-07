dc05ede64285c22224f33391fbd22fbef51bbcfd
juleszweekhorst <57524793+juleszweekhorst@users.noreply.github.com>
2021-10-07 12:17:22 +0200

    Update example.py

--- example.py
+++ example.py
@@ -1,4 +1,7 @@
-temperature = 10
-def print_temperature():
-    print(temperature)
-   
+def add(a, b):
+    return a + b
+
+
+def test_add():
+    result = add(0.1,0.2)
+    assert abs(result-0.3) <1.0e-7
